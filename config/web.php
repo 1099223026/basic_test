78a792b2998f356aba1eb3d3d4a039c5e0f6fe0d
1099223026 <1099223026@qq.com>
2017-10-29 11:12:22 +0800

    1212

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '3524sad3f2as14d56fa1sd635f1',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
